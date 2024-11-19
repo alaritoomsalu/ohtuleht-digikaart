8f18cbbf5577441e5233e1e08b4cdd10e03d2bc4
alaritoomsalu <alari.toomsalu@gmail.com>
2024-11-20 00:20:35 +0200

    edit

---
+++ build/script.js
@@ -0,0 +1,27 @@
+async function sendWish() {
+    try {
+        const response = await fetch("https://ohtuleht-digikaart-c2f2856dfe57.herokuapp.com/count-wish", {
+            method: "POST",
+        });
+
+        if (!response.ok) {
+            alert("Midagi läks valesti!");
+            return;
+        }
+
+        gtag("event", "wish_sent", {
+            event_category: "Engagement",
+            event_label: "Wish Submission",
+            value: 1,
+        });
+
+        // Clear textarea and switch cards
+        document.querySelector(".input-field").value = "";
+        document.querySelector(".first-card").classList.add("hidden");
+        document.querySelector(".second-card").classList.remove("hidden");
+
+    } catch (error) {
+        console.error("Viga soovi saatmisel:", error);
+        alert("Serveriga ühendamine ebaõnnestus!");
+    }
+}
